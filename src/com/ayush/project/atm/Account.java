9a76bd439c45179a434d89bc3c46028c6788dc28
ig-ayush <igayushacharya@gmail.com>
2025-11-13 02:34:47 +0530

    Finished the account object

--- src/com/ayush/project/atm/Account.java
+++ src/com/ayush/project/atm/Account.java
@@ -1,5 +1,59 @@
 package com.ayush.project.atm;
 
-public class Account {
+import java.util.Scanner;
 
+public abstract class Account {
+	
+	private int accno;
+	private int pin;
+	private double balance;
+	
+	public Account(int accno, int pin, double balance) {
+		this.accno = accno;
+		this.pin = pin;
+		this.balance = balance;
+	}
+	
+	public boolean checkPIN(int inputPIN) {
+		if(inputPIN == pin) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int getAccountNo() {
+		return accno;
+	}
+	
+	public void Deposit(double amount) {
+		if(amount <= 0) {
+			System.out.println("ENTER VALID AMOUNT TO DEPOSIT");
+			return;
+		}
+		
+		balance += amount;
+		System.out.println("YOUR ₹" + amount + " IS DEPOSIT TO YOUR ACCOUNT.");
+	}
+	
+	@SuppressWarnings("resource")
+	public void withdraw(double amount) {
+		Scanner sc = new Scanner(System.in); 
+		if(amount > balance) {
+			System.out.println("INSUFFICIENT BALANCE");
+		} else if(amount == balance) {
+			System.out.println("THIS WITHDRAW MAKE YOUR BALANCE 0");
+			System.out.println("WANT TO CONTINUE (Y/N)");
+			String no = sc.next();
+			if(no.equalsIgnoreCase("no") || no.equalsIgnoreCase("n")) {
+				return;
+			}
+			
+			balance -= amount;
+			System.out.println("YOUR ₹" + amount + " IS WITHDRAW FROM YOUR ACCOUNT.");
+		}
+		
+		sc.close();
+	}
+	
+	public abstract String getAccountType();
 }
