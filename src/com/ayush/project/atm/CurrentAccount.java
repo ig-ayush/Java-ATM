ab5489c5e89aba984a3e35583d621bd3f40da1eb
ig-ayush <igayushacharya@gmail.com>
2025-11-13 02:41:58 +0530

    Completed the current account code

--- src/com/ayush/project/atm/CurrentAccount.java
+++ src/com/ayush/project/atm/CurrentAccount.java
@@ -1,5 +1,12 @@
 package com.ayush.project.atm;
 
-public class CurrentAccount {
-
+public class CurrentAccount extends Account{
+	
+	public CurrentAccount(int accno, int pin, double balance) {
+		super(accno, pin, balance);
+	}
+	
+	public String getAccountType() {
+		return "CURRENT ACCOUNT";
+	}
 }
