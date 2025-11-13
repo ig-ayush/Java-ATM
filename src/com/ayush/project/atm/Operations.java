fe3fcbac8fdcde2c40c45cf54c00557734d596ed
ig-ayush <igayushacharya@gmail.com>
2025-11-13 19:46:04 +0530

    start the all operations functions

--- src/com/ayush/project/atm/Operations.java
+++ src/com/ayush/project/atm/Operations.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class Operations {
 	
 	Map<Integer, Account> account = new HashMap<>();
+	Scanner sc = new Scanner(System.in);
 	
 	public Operations() {
 		account.put(1010, new SavingAccount("Ayush Acharya", 1510, 2000));
 		account.put(1020, new CurrentAccount("Jeevan Shetty", 1015, 3000));
 	}
 	
+	//	START FUNCTION
 	public void start() {
-		
-		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("WELCOME TO JAVA ATM");
 		
@@ -48,9 +48,66 @@ public class Operations {
 			} 
 			
 			sc.close();
-			System.out.println("WELCOME!!");
+			
+			menu(acc);
+		}
+		
+		
+	}
+	
+	//DEPOSIT	
+	public void deposit(Account acc) {
+		double amount = sc.nextDouble();
+		acc.Deposit(amount);
+		System.out.println("YOUR PRESENT BALANCE IS: " + acc.balance);
+	}
+	
+	//WITHDRAW	
+	public void withdraw(Account acc) {
+		double amount = sc.nextDouble();
+		acc.withdraw(amount);
+		System.out.println("YOUR PRESENT BALANCE IS: " + acc.balance);
+	}
+	
+	//CHECK DETAILS
+	public void checkDetails(Account acc) {
+		String name = acc.name;
+		double balance = acc.balance;
+		System.out.println("ACCOUNT HOLDER: " + name);
+		System.out.println("ACCOUNT BALANCE: " + balance);
+	}
+	
+	//MENU FUNCTION
+	public void menu(Account acc) {
+		
+		while(true) {
+			System.out.println("LOGGED IN: " + acc.name + " ACCOUNT (" + acc.getAccountType() + ")");
+			System.out.println("1] DEPOSIT");
+			System.out.println("2] WITHDRAW");
+			System.out.println("3] CHECK DETAILS");
+			System.out.println("4] LOGOUT");
+			System.out.println("CHOOSE YOUT OPTION:");
+			
+			char choice = sc.next().charAt(0);
+			
+			switch(choice) {
+			
+				case '1' : deposit(acc);
+				break;
+				
+				case '2' : withdraw(acc);
+				break;
+				
+				case '3' : checkDetails(acc);
+				break;
+				
+				case '4' : System.out.println("THANK YOU!!");
+				break;
+			}
+			
 			
 		}
+		
 	}
 	
 }
